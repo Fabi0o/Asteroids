10ddad5ee0ba20f7c073b08bab2d1fb34b572525
Fabi0o <marfab32@gmail.com>
2025-07-10 12:22:00 +0200

    feat: render player on screen

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-   pygame.init()
-   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
-   while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            dt = clock.tick(60) / 1000
             
-    screen.fill("#000000")
-    pygame.display.flip()
+        screen.fill("#000000")
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
